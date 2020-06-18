587d08b083beddaa31db694557cd667e190e66b7
Firefouks <haddad.b.pro@gmail.com>
2020-06-18 18:50:56 +0200

    first render

--- Learning.React/Structure/app.js
+++ Learning.React/Structure/app.js
@@ -1,22 +1,32 @@
-function FilterableProductTable()
-{
-    const products = [
-        {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-        {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-        {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-        {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-        {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-        {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-    ];
+// Je n'utilise ni d'import, ni de hooks car je n'utilise pas webpack, j'ai voulu développer quelque chose rapidement :) sinon je peux toujours regarder ce tuto https://medium.com/@bouraine/create-a-react-app-from-scratch-cea5b76a1e1b
+const products = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"},
+    {category: "xxx", price: "$199.99", stocked: true, name: "Nexus 7"}
+]
 
-    // Pour que cette ligne fonctionne, il faudra que je suive le tutoriel de mohamed bouraoine
-    // - https://medium.com/@bouraine/create-a-react-app-from-scratch-cea5b76a1e1b
-    const [filteredProducts, setFilteredProducts] = useState(products);
-    
-    return <div className="container col-4">
-        <ProductTableFilters/>
-        <ProductTable/>
-    </div>
+class FilterableProductTable extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            filteredProducts : products
+        }
+    }
+
+    render()
+    {
+        return <div className="container col-4">
+            <ProductTableFilters/>
+            <ProductTable products={this.state.filteredProducts}/>
+        </div>
+    }
 }
 
 function ProductTableFilters()
@@ -45,20 +55,51 @@ const Checkbox = React.memo(function({name, checked = false, onChange = () => {}
 })
 
 
-const ProductTable = React.memo(function()
+const ProductTable = React.memo(function({products})
 {
+    let lastCategory = null
+    const rows = []
+    
+    console.log(products)
+    products.forEach(p =>
+    {
+        if (lastCategory != p.category)
+        {
+            lastCategory = p.category
+            rows.push(<ProductCategoryRow key={p.category} product={p}/>)
+        }
+
+        rows.push(<ProductRow key={p.name} product={p}/>)
+    })
+
     return <div>
-        <table className="table mt-4">
+        <table className="table table-dark table-striped mt-4">
             <thead>
                 <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
+                    <th>Name</th>
+                    <th>Price</th>
                 </tr>
             </thead>
-            <tbody>
-            </tbody>
+            <tbody>{rows}</tbody>
         </table>
     </div>
+})
+
+const ProductCategoryRow = React.memo(function({product})
+{
+    return <tr>
+        <th colSpan="2">{product.category}</th>
+    </tr>
+})
+
+const ProductRow = React.memo(function({product})
+{
+    const nameClasses = product.stocked ? '' : 'text-danger';
+
+    return <tr>
+        <td className={nameClasses}>{product.name}</td>
+        <td>{product.price}</td>
+    </tr>
 })
 
 ReactDOM.render(
