2c83aa33c30a42fbbc432059985e481d137fcefd
Firefouks <haddad.b.pro@gmail.com>
2020-08-08 15:49:46 +0200

    css

--- Learning.RealTimeApplication/Front-end/chat/src/components/Chat/Chat.tsx
+++ Learning.RealTimeApplication/Front-end/chat/src/components/Chat/Chat.tsx
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
         height : '52px',
         float : 'right'
     },
-    listMessageItem :
+    messagesList :
     {
         width: '100%',
         backgroundColor: theme.palette.background.paper,
@@ -40,8 +40,17 @@ const useStyles = makeStyles((theme) => ({
         display: 'inline',
     },
 }));
+
+interface IMessage
+{
+   username : string,
+   message : string
+}
+
 const Chat = () =>
 {
+    const classes = useStyles();
+
     {/* Send a message broadcoast*/}
     const [message, setMessage] = useState<string>('');
     const [username, setUsername] = useState<string>('');
@@ -50,7 +59,46 @@ const Chat = () =>
     function onChangeUsername(event : ChangeEvent<HTMLInputElement>) : void { setUsername(event.target.value); }
 
     {/* List messages */}
-    const classes = useStyles();
+    const [messages, setMessages] = useState<IMessage[]>([
+        {
+            username : 'Alex',
+            message : ' I\'ll be in your neighborhood doing errands this…'
+        },
+        {
+            username : 'Thomas',
+            message : ' Wish I could come, but I\'m out of town this…'
+        },
+        {
+            username : 'Casandra',
+            message : 'Do you have Paris recommendations? Have you ever…'
+        },
+    ]);
+    const messagesComponent = messages.map(m => {
+        const imagePath = `/static/images/avatar/${Math.floor(Math.random() * Math.floor(3)) + 1}.jpg`
+        return (<>
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src={imagePath} />
+            </ListItemAvatar>
+            <ListItemText
+                primary={m.username}
+                secondary={
+                    <React.Fragment>
+                        <Typography
+                            component="span"
+                            variant="body2"
+                            className={classes.inline}
+                            color="textPrimary"
+                        >
+                        </Typography>
+                        {m.message}
+                    </React.Fragment>
+                }
+            />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        </>)
+    });
 
     return (
         <div>
@@ -85,72 +133,8 @@ const Chat = () =>
                     {/* List messages */}
                     <br/>
                     <br/>
-                    <List className={classes.listMessageItem}>
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="Brunch this weekend?"
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            className={classes.inline}
-                                            color="textPrimary"
-                                        >
-                                            Ali Connors
-                                        </Typography>
-                                        {" — I'll be in your neighborhood doing errands this…"}
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
-                        <Divider variant="inset" component="li" />
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="Summer BBQ"
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            className={classes.inline}
-                                            color="textPrimary"
-                                        >
-                                            to Scott, Alex, Jennifer
-                                        </Typography>
-                                        {" — Wish I could come, but I'm out of town this…"}
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
-                        <Divider variant="inset" component="li" />
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary="Oui Oui"
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            className={classes.inline}
-                                            color="textPrimary"
-                                        >
-                                            Sandra Adams
-                                        </Typography>
-                                        {' — Do you have Paris recommendations? Have you ever…'}
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
+                    <List className={classes.messagesList}>
+                        {messagesComponent}
                     </List>
                 </Container>
             </React.Fragment>
