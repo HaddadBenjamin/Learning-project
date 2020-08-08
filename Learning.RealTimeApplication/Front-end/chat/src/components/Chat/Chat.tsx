a9c87550ccdbcd6bd6ed89f96dcfdc5e2dd370aa
Firefouks <haddad.b.pro@gmail.com>
2020-08-08 15:10:28 +0200

    materialize

--- Learning.RealTimeApplication/Front-end/chat/src/components/Chat/Chat.tsx
+++ Learning.RealTimeApplication/Front-end/chat/src/components/Chat/Chat.tsx
@@ -4,11 +4,52 @@ import React, {
     useState
 } from "react";
 
+import {TextField, Container, Typography, CssBaseline, Button} from "@material-ui/core"
 const Chat = () =>
 {
+    const [message, setMessage] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+
+    function onChangeMessage(event : ChangeEvent<HTMLInputElement>) : void
+    {
+        setMessage(event.target.value);
+    }
+
+    function onChangeUsername(event : ChangeEvent<HTMLInputElement>) : void
+    {
+        setUsername(event.target.value);
+    }
+
     return (
         <div>
-            Chat
+            <React.Fragment>
+                <CssBaseline />
+                <Container fixed>
+                    <br/>
+                    <TextField
+                        id="outlined-textarea"
+                        label="Username"
+                        placeholder="Username"
+                        variant="outlined"
+                        value={username}
+                        onChange={onChangeUsername}
+                        style={{'width' : '20%'}}
+                    />
+                    <TextField
+                        id="outlined-textarea"
+                        label="Message"
+                        placeholder="Message"
+                        multiline
+                        variant="outlined"
+                        value={message}
+                        onChange={onChangeMessage}
+                        style={{width : '60%', marginLeft : '5px'}}
+                    />
+                    <Button variant="contained" color="secondary" style={{marginLeft : '10px', height : '52px'}}>
+                        Send message
+                    </Button>
+                </Container>
+            </React.Fragment>
         </div>
     )
 };
