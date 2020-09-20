4b0ab2a2d07d4b52d14fc7fb8ae5802f91415384
Firefouks <haddad.b.pro@gmail.com>
2020-09-20 16:14:22 +0200

    dependency injection wip

---
+++ Learning.NETCore/Mediator/Back-end/Learning.Mediator.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Learning.Mediator.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterMediator(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            // enregistrer toutes les dépendances génériques des handlers (command, query, event).
+            return services.AddSingleton<IMediator, Mediator>();
+        }
+
+        public static IServiceCollection RegisterMediator(this IServiceCollection services, params Type[] types) =>
+            RegisterMediator(services, types.Select(t => t.Assembly).ToArray());
+    }
+}
