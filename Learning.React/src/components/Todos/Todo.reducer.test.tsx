ab13eedb37dbdef6d8b514a7cdc8734c350c3e4c
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-03-05 17:49:44 +0100

    some nested tests

--- Learning.React/src/components/Todos/Todo.reducer.test.tsx
+++ Learning.React/src/components/Todos/Todo.reducer.test.tsx
@@ -5,6 +5,7 @@ import { createTodo, deleteTodo, TodoActions, toggleTodo, updateTodo, updateTodo
 describe("TodoReducer", () =>
 {
   it("CREATE_TODO should create a new todo", () => {
+        // Arrange
         const initialState : ITodosState = { todos : [], filters : { onlyUncompleted : false, terms : '' }}
         const newTodoTitle : string = 'Faire les courses'
         const createTodoAction : TodoActions = createTodo(newTodoTitle)
@@ -18,59 +19,103 @@ describe("TodoReducer", () =>
         expect(newState.todos[0].title).toBe(newTodoTitle)
     }),
 
-    it("UPDATE_TODO should update an existing todo", () => {
-        const existingTodo : Todo = { id : '1', completed : false, title : 'Faire les courses'}
-        const initialState : ITodosState =
-        {
-            todos : [ existingTodo ],
-            filters : { onlyUncompleted : false, terms : '' }
-        }
-        const newTodoTitle : string = 'Acheter du poivre'
-        const updateTodoAction : TodoActions = updateTodo(existingTodo.id, newTodoTitle)
+    describe("UPDATE_TODO", () => 
+    {
+        it("UPDATE_TODO should update an existing todo", () => {
+            // Arrange
+            const existingTodo : Todo = { id : '1', completed : false, title : 'Faire les courses'}
+            const initialState : ITodosState =
+            {
+                todos : [ existingTodo ],
+                filters : { onlyUncompleted : false, terms : '' }
+            }
+            const newTodoTitle : string = 'Acheter du poivre'
+            const updateTodoAction : TodoActions = updateTodo(existingTodo.id, newTodoTitle)
+    
+            // Act
+            const newState : ITodosState = todoReducer(initialState, updateTodoAction)
+    
+            // Assert
+            expect(newState.todos.length).toBe(1)
+            expect(newState.todos[0].completed).toBeFalsy()
+            expect(newState.todos[0].title).toBe(newTodoTitle)
+        }),
 
-        // Act
-        const newState : ITodosState = todoReducer(initialState, updateTodoAction)
+        it("UPDATE_TODO should not throw an excpetion when the todo don't exists", () => {
+            // Arrange
+            const initialState : ITodosState = { todos : [], filters : { onlyUncompleted : false, terms : '' }}
+            const todoIdThatDontExists : string = '28'
+            const newTodoTitle : string = 'Acheter du poivre'
+            const updateTodoAction : TodoActions = updateTodo(todoIdThatDontExists, newTodoTitle)
 
-        // Assert
-        expect(newState.todos.length).toBe(1)
-        expect(newState.todos[0].completed).toBeFalsy()
-        expect(newState.todos[0].title).toBe(newTodoTitle)
+             // Act & Assert
+             expect(() => todoReducer(initialState, updateTodoAction)).not.toThrow()
+        })
     }),
 
-    it("TOGGLE_TODO should toggle an existing todo", () => {
-        const existingTodo : Todo = { id : '1', completed : false, title : 'Faire les courses'}
-        const initialState : ITodosState =
-        {
-            todos : [ existingTodo ],
-            filters : { onlyUncompleted : false, terms : '' }
-        }
-        const toggleTodoAction : TodoActions = toggleTodo(existingTodo.id)
+    describe("TOGGLE_TODO", () => 
+    {
+        it("TOGGLE_TODO should toggle an existing todo", () => {
+            // Arrange
+            const existingTodo : Todo = { id : '1', completed : false, title : 'Faire les courses'}
+            const initialState : ITodosState =
+            {
+                todos : [ existingTodo ],
+                filters : { onlyUncompleted : false, terms : '' }
+            }
+            const toggleTodoAction : TodoActions = toggleTodo(existingTodo.id)
+    
+            // Act
+            const newState : ITodosState = todoReducer(initialState, toggleTodoAction)
+    
+            // Assert
+            expect(newState.todos.length).toBe(1)
+            expect(newState.todos[0].completed).toBeTruthy()
+        }),
 
-        // Act
-        const newState : ITodosState = todoReducer(initialState, toggleTodoAction)
+        it("TOGGLE_TODO should not throw an excpetion when the todo don't exists", () => {
+            // Arrange
+            const initialState : ITodosState = { todos : [], filters : { onlyUncompleted : false, terms : '' }}
+            const todoIdThatDontExists : string = '28'
+            const toggleTodoAction : TodoActions = toggleTodo(todoIdThatDontExists)
 
-        // Assert
-        expect(newState.todos.length).toBe(1)
-        expect(newState.todos[0].completed).toBeTruthy()
+             // Act & Assert
+             expect(() => todoReducer(initialState, toggleTodoAction)).not.toThrow()
+        })
     }),
 
-    it("DELETE_TODO should delete an existing todo", () => {
-        const existingTodo : Todo = { id : '1', completed : false, title : 'Faire les courses'}
-        const initialState : ITodosState =
-        {
-            todos : [ existingTodo ],
-            filters : { onlyUncompleted : false, terms : '' }
-        }
-        const deleteTodoAction : TodoActions = deleteTodo(existingTodo.id)
+    describe("DELETE_TODO", () => 
+    {    
+        it("DELETE_TODO should delete an existing todo", () => {
+            // Arrange
+            const existingTodo : Todo = { id : '1', completed : false, title : 'Faire les courses'}
+            const initialState : ITodosState =
+            {
+                todos : [ existingTodo ],
+                filters : { onlyUncompleted : false, terms : '' }
+            }
+            const deleteTodoAction : TodoActions = deleteTodo(existingTodo.id)
 
-        // Act
-        const newState : ITodosState = todoReducer(initialState, deleteTodoAction)
+            // Act
+            const newState : ITodosState = todoReducer(initialState, deleteTodoAction)
 
-        // Assert
-        expect(newState.todos.length).toBe(0)
-    }),
+            // Assert
+            expect(newState.todos.length).toBe(0)
+        }),
+
+        it("DELETE_TODO should not throw an excpetion when the todo don't exists", () => {
+            // Arrange
+            const initialState : ITodosState = { todos : [], filters : { onlyUncompleted : false, terms : '' }}
+            const todoIdThatDontExists : string = '28'
+            const deleteTodoAction : TodoActions = deleteTodo(todoIdThatDontExists)
+
+             // Act & Assert
+             expect(() => todoReducer(initialState, deleteTodoAction)).not.toThrow()
+        })
+    })
 
     it("UPDATE_TODO_FILTERS should update to do filters", () => {
+        // Arrange
         const initialState : ITodosState = { todos : [], filters : { onlyUncompleted : false, terms : '' }}
         const newTodoFilters : ITodoFilters = { onlyUncompleted : true, terms : 'courses' }
         const updateTodoFiltersAction : TodoActions = updateTodoFilters(newTodoFilters)
