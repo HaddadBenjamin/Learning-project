963559080203c4a453e94ca73c02c5ca9cf359ad
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-03-10 15:38:42 +0100

    actioin & fix api

--- Learning.React/src/components/Todos/todo.action.tsx
+++ Learning.React/src/components/Todos/todo.action.tsx
@@ -3,15 +3,29 @@ import actionCreatorFactory from 'typescript-fsa'
 
 export enum TodoActions
 {
+    GET_TODO = "todo/get",
+    GET_TODO_STARTED = "todo/get_STARTED",
+ 
     CREATE_TODO = "todo/create",
     CREATE_TODO_STARTED = "todo/create_STARTED",
 
     UPDATE_TODO = "todo/update",
-    TOGGLE_TODO = "todo/toggle",
-    DELETE_TODO = "todo/delete",
+    UPDATE_TODO_STARTED = "todo/update_STARTED",
 
-    UPDATE_TODO_FILTERS = "todo/update/filters"
+    TOGGLE_TODO = "todo/toggle",
+    TOGGLE_TODO_STARTED = "todo/toggle_STARTED",
+
+    DELETE_TODO = "todo/delete",
+    DELETE_TODO_STARTED = "todo/delete_STARTED",
+
+    UPDATE_TODO_FILTERS = "todo/update/filters",
+    UPDATE_TODO_FILTERS_STARTED = "todo/update/filters_STARTED"
 }
+export interface IGetTodoAction { }
+export interface IGotTodosAction { todos : ITodo[] }
+export interface IGetTodoFailedAction { errorMessage : string }
+export const getTodos = actionCreatorFactory().async<
+    IGetTodoAction, IGotTodosAction, IGetTodoFailedAction>(TodoActions.GET_TODO)
 
 export interface ICreateTodoAction { title : string }
 export interface ICreatedTodoAction { todo : ITodo }
@@ -20,13 +34,22 @@ export const createTodo = actionCreatorFactory().async<
     ICreateTodoAction, ICreatedTodoAction, ICreateTodoFailedAction>(TodoActions.CREATE_TODO)
 
 export interface IUpdateTodoAction { id : string, newTitle : string }
-export const updateTodo = actionCreatorFactory()<IUpdateTodoAction>(TodoActions.UPDATE_TODO)
+export interface IUpdatedTodoAction { todo : ITodo }
+export interface IUpdateTodoFailedAction { errorMessage : string }
+export const updateTodo = actionCreatorFactory().async<
+    IUpdateTodoAction, IUpdatedTodoAction, IUpdateTodoFailedAction>(TodoActions.UPDATE_TODO)
 
 export interface IToggleTodoAction { id : string }
-export const toggleTodo = actionCreatorFactory()<IToggleTodoAction>(TodoActions.TOGGLE_TODO)
+export interface IToggledTodoAction { todo : ITodo }
+export interface IToggleTodoFailedAction { errorMessage : string }
+export const toggleTodo = actionCreatorFactory().async<
+    IToggleTodoAction, IToggledTodoAction, IToggleTodoFailedAction>(TodoActions.TOGGLE_TODO)
 
 export interface IDeleteTodoAction { id : string }
-export const deleteTodo = actionCreatorFactory()<IDeleteTodoAction>(TodoActions.DELETE_TODO)
+export interface IDeletedTodoAction { id : string }
+export interface IDeleteTodoFailedAction { errorMessage : string }
+export const deleteTodo = actionCreatorFactory().async<
+    IDeleteTodoAction, IDeletedTodoAction, IDeleteTodoFailedAction>(TodoActions.DELETE_TODO)
 
 export interface IUpdateTodoFiltersAction { filters : ITodoFilters }
 export const updateTodoFilters = actionCreatorFactory()<IUpdateTodoFiltersAction>(TodoActions.UPDATE_TODO_FILTERS)
--- Learning.React/src/components/Todos/todo.api.tsx
+++ Learning.React/src/components/Todos/todo.api.tsx
@@ -8,6 +8,10 @@ export const getTodos = () : AxiosPromise<ITodo[]> => axios.get(api)
 
 export const createTodo = (todo : ITodo) : AxiosPromise<ITodo> => axios.post(api, JSON.stringify(todo))
 
-export const updateTodo = (todo : ITodo) : AxiosPromise<ITodo> => axios.put(`${api}/${todo.id}`, JSON.stringify(todo))
+export const patchTodoTitle = (id : string, title : string) : AxiosPromise<ITodo> => 
+    axios.patch(`${api}/${id}`, JSON.stringify({ title : title }))
+
+export const patchTodoCompleted = (id : string, completed : boolean) : AxiosPromise<ITodo> => 
+    axios.patch(`${api}/${id}`, JSON.stringify({ completed : completed }))
 
 export const deleteTodo = (id : string)  : AxiosPromise => axios.delete(`${api}/${id}`)
