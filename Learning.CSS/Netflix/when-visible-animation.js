a1aba9d22ef0a2b54a98c238bfaa65ca91f290f8
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-05-04 11:43:51 +0200

    animate netflix

---
+++ Learning.CSS/Netflix/when-visible-animation.js
@@ -0,0 +1,22 @@
+const observerOptions =
+{
+    root : null,
+    rootMargin : '0px',
+    threshold : 0.1
+}
+
+const onPartiallyVisible = function(entries, observer)
+{
+    entries.forEach(entry =>
+    {
+        if (entry.intersectionRatio > observerOptions.threshold)
+        {
+            entry.target.classList.add('reveal-container-visible');
+            observer.unobserve(entry.target);
+        }
+    })
+}
+
+const revealContainerObserver = new IntersectionObserver(onPartiallyVisible, observerOptions)
+document.querySelectorAll('.reveal-container')
+        .forEach(revealContainer => revealContainerObserver.observe(revealContainer))
