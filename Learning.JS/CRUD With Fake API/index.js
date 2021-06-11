c2bf98e440c2bbe4bc8fd2ef27f2461adb1faa43
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-06-11 16:55:28 +0200

    refactoring

--- Learning.JS/CRUD With Fake API/index.js
+++ Learning.JS/CRUD With Fake API/index.js
@@ -1,118 +1,165 @@
 (async () =>
 {
-    const todoLists = document.querySelectorAll('.done-todo-list, .undone-todo-list')
-    const cleanTodos = () => todoLists.forEach(t => t.innerHTML = '')
-
-    const apiUrl = "http://localhost:3004/todos"
-    const generateTodoLists = async () =>
+    class TodoApi
     {
-        const todos = await (await fetch(apiUrl)).json();
-       
-        todos.forEach(todo => addTodo(todo))
+        apiUrl = "http://localhost:3004/todos"
+        headers = { "Content-Type" : "application/json" }
+
+        getAll = async () => await (await fetch(this.apiUrl)).json()
+
+        create = async(todo) => await (await fetch(this.apiUrl,
+        { 
+            method : 'POST',
+            headers : this.headers,
+            body : JSON.stringify(todo)
+        }))
+
+        patchCompleted = async (id, completed) =>  await (await fetch(`${this.apiUrl}/${id}`,
+        {
+            method : 'PATCH',
+            headers : this.headers,
+            body : JSON.stringify({ completed : !completed }),
+        }))
+
+        delete = async (id) => await (await fetch(`${this.apiUrl}/${id}`, { method : 'DELETE' }))
     }
 
+    class TodoState
+    {
+        todos = []
+
+        set = (todos) => this.todos = todos
+
+        add = (todo) => this.todos.push(todo)
+
+        toggle = (id) => this.todos = this.todos.map(todo => todo.id === id ? { ...todo, completed : !todo.completed } : todo)
+
+        delete = (id) => this.todos = this.todos.filter(todo => todo.id === id )
+
+        generateId = () => Math.max(this.todos.map(todo => todo.id)) + 1
+    }
+
+    class TodoDom
+    {
+        add = ({ id, content, completed }, resetInput = false) =>
+        {
+            let newTodo = document.createElement('div')
+            let toggleClass = completed ? 'todo-toggle-checked' : 'todo-toggle'
+            let todoList = completed ? doneTodoList : undoneTodoList
+
+            newTodo.innerHTML = `
+                <li class="todo" data-todo-id="${id}">
+                    <div class="${toggleClass}">
+                        <div class="todo-toggle-checked-image"></div>
+                    </div>
+                    <div class="todo-content">${content}</div>
+                    <div class="todo-remove"></div>
+                </li>`
+
+            todoList.appendChild(newTodo)  
+
+            newTodo.querySelector('.todo-remove').addEventListener('click', todoEventHandlers.remove)
+            newTodo.querySelector('.todo-toggle, .todo-toggle-checked').addEventListener('click', todoEventHandlers.toggle)
+
+            if (resetInput)
+            {
+                let addContentInput = this.getAddContentInput()
+                
+                addContentInput.value = addContentInput.placeholder = ''
+            }
+        }
+
+        toggle = (toggle, completed, todo) =>
+        {
+            toggle.classList.toggle('todo-toggle')
+            toggle.classList.toggle('todo-toggle-checked')
+
+            const todoList = completed ? doneTodoList : undoneTodoList
+            
+            todoList.appendChild(todo)
+        }
+
+        expandOrCollapse = () =>
+        {
+            let doneTodoListImage = document.querySelector('.arrow-down-image') 
+      
+            doneTodoList.classList.toggle('hidden')
+    
+            doneTodoListImage.classList.toggle('arrow-down-image')
+            doneTodoListImage.classList.toggle('arrow-right-image')
+        }
+
+        delete = (todo) => todo.parentElement.removeChild(todo)
+
+        isCompleted = (toggle) => toggle.classList.contains('todo-toggle-checked')
+        isTodo = (element) => element.classList.contains('todo')
+
+        getAddContentInput = () => document.querySelector('.add-todo-content')
+    }
+
+    class TodoEventHandlers
+    {
+        add = async () =>
+        {
+            let content = todoDom.getAddContentInput().value
+
+            if (content === '') return
+
+            let id = todoState.generateId()
+            let todo = { id, content, completed : false }
+
+            todoDom.add(todo, true)
+            todoState.add(todo)
+            await todoApi.create(todo)
+        }
+
+        toggle = async (event) =>
+        {
+            const toggle = todoDom.isTodo(event.target.parentElement) ? event.target : event.target.parentElement
+            const completed = todoDom.isCompleted(toggle)
+            const todo = toggle.parentElement
+            const id = todo.dataset.todoId
+            
+            todoDom.toggle(toggle, completed, todo)
+            todoState.toggle(id)
+            await todoApi.patchCompleted(id, completed)
+        }
+
+        remove = async (event) => 
+        {
+            const todo = event.target.parentElement
+            const id = todo.dataset.todoId
+            
+            todoDom.delete(todo)
+            todoState.delete(id)
+            await todoApi.delete(id)
+        }
+
+        expandOrCollapseCompletedTodos = () => todoDom.expandOrCollapse()
+    }
+
+    let todoApi = new TodoApi()
+    let todoState = new TodoState()
+    let todoDom = new TodoDom()
+    let todoEventHandlers = new TodoEventHandlers()
     let doneTodoList = document.querySelector('.done-todo-list')
     let undoneTodoList = document.querySelector('.undone-todo-list')
-    const addTodo = ({ content, completed }) =>
-    {
-        let newTodo = document.createElement('div')
-        let toggleClass = completed ? 'todo-toggle-checked' : 'todo-toggle'
-        let todoList = completed ? doneTodoList : undoneTodoList
-
-        newTodo.innerHTML = `
-            <li class="todo">
-                <div class="${toggleClass}">
-                    <div class="todo-toggle-checked-image"></div>
-                </div>
-                <div class="todo-content">${content}</div>
-                <div class="todo-remove"></div>
-            </li>`
-
-        todoList.appendChild(newTodo)  
-
-        newTodo.querySelector('.todo-remove').addEventListener('click', removeTodoEventHandler)
-        newTodo.querySelector('.todo-toggle, .todo-toggle-checked').addEventListener('click', toggleTodoEventHandler)
-    }
-
-    // Toggle todo
-    let todoToggles = document.querySelectorAll('.todo-toggle, .todo-toggle-checked')
-
-    const toggleTodoEventHandler = (event) =>
-    {
-        let toggle = event.target.parentElement.classList.contains('todo') ? event.target : event.target.parentElement
-        let todo = toggle.parentElement
-        
-        toggle.classList.toggle('todo-toggle')
-        toggle.classList.toggle('todo-toggle-checked')
-
-        const toggled = toggle.classList.contains('todo-toggle-checked')
-        const todoList = toggled ? doneTodoList : undoneTodoList
-        
-        todoList.appendChild(todo)
-    }
-
-    todoToggles.forEach(toggle => toggle.addEventListener('click', toggleTodoEventHandler))
-
-
-    // Remove todo
-    let removeTodos = document.querySelectorAll('.todo-remove') // delete-icon
-
-    const removeTodoEventHandler = (event) => 
-    {
-        const todo = event.target.parentElement
-        const todoList = todo.parentElement
-
-        todoList.removeChild(todo)
-    }
-
-    removeTodos.forEach(remove => remove.addEventListener('click', removeTodoEventHandler))
-
 
     // Expand & collapse done todo list
-    let doneTodoListImage = document.querySelector('.arrow-down-image') 
     let doneTodoListTitle = document.querySelector('.done-todo-list-title') 
-
-    const doneTodosTextEventHandler = () =>
-    {
-        doneTodoList.classList.toggle('hidden')
-
-        doneTodoListImage.classList.toggle('arrow-down-image')
-        doneTodoListImage.classList.toggle('arrow-right-image')
-    }
-
-    doneTodoListTitle.addEventListener('click', doneTodosTextEventHandler)
-
+    doneTodoListTitle.addEventListener('click', todoEventHandlers.expandOrCollapseCompletedTodos)
 
     // Add todo
     let addTodoImage = document.querySelector('.add-todo-image')
-    let addTodoContentInput = document.querySelector('.add-todo-content')
-    const addTodoEventHandler = () =>
+    addTodoImage.addEventListener('click', todoEventHandlers.add)
+
+    // Generate to do list
+    const generateTodoList = async () =>
     {
-        if (addTodoContentInput.value === '') return
-
-        let todoContent = addTodoContentInput.value
-        let newTodo = document.createElement('div')
-        
-        newTodo.innerHTML = `
-            <li class="todo">
-                <div class="todo-toggle">
-                    <div class="todo-toggle-checked-image"></div>
-                </div>
-                <div class="todo-content">${todoContent}</div>
-                <div class="todo-remove"></div>
-            </li>`
-
-        undoneTodoList.appendChild(newTodo)  
-
-        newTodo.querySelector('.todo-remove').addEventListener('click', removeTodoEventHandler)
-        newTodo.querySelector('.todo-toggle').addEventListener('click', toggleTodoEventHandler)
-
-        addTodoContentInput.value = addTodoContentInput.placeholder = ''
+        todoState.set(await todoApi.getAll())
+        todoState.todos.forEach(todo => todoDom.add(todo))
     }
 
-    addTodoImage.addEventListener('click', addTodoEventHandler)
-
-    cleanTodos()
-    await generateTodoLists()
+    await generateTodoList()
 })()
 
